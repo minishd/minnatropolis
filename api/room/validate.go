99456ff54a9b4b77daac7587c33b5186de1a2bf0
Aunarky <54250170+Aunarky@users.noreply.github.com>
2026-08-24 19:40:48 +0300

    feat: last bit of validation and credits

--- api/room/validate.go
+++ api/room/validate.go
@@ -6,6 +6,7 @@ import (
 	pt "github.com/minishd/minnatropolis/api/room/protocol"
 )
 
+// Ranges come from ynoproject/ynoserver
 const (
 	minRoomID = 1
 	maxRoomID = 5000
@@ -72,6 +73,32 @@ func (h *Handler) validateMessage(m any) error {
 			return fmt.Errorf("facing direction %d out of range", m.Direction)
 		}
 
+	case pt.SpeedC2S:
+		if !isValidSpeed(m.Speed) {
+			return fmt.Errorf("speed %d out of range", m.Speed)
+		}
+
+	case pt.SpriteC2S:
+		// TBD
+
+	case pt.MainPlayerPosC2S:
+		// TBD
+
+	case pt.TransparencyC2S:
+		if !isValidTransparency(m.Transparency) {
+			return fmt.Errorf("transparency %d out of range", m.Transparency)
+		}
+
+	case pt.SoundEffectC2S:
+		if !isValidSoundVolume(m.Volume) {
+			return fmt.Errorf("sound volume %d out of range", m.Volume)
+		}
+		if !isValidSoundTempo(m.Tempo) {
+			return fmt.Errorf("sound tempo %d out of range", m.Tempo)
+		}
+		if !isValidSoundBalance(m.Balance) {
+			return fmt.Errorf("sound balance %d out of range", m.Balance)
+		}
 	}
 
 	return nil
